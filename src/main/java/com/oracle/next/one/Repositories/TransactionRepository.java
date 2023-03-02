d60f6ae63603cc8fde0d012a5a343384fab77e4d
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2023-03-02 08:21:59 -0300

    Feat: Repositório para as transações de conversões

---
+++ src/main/java/com/oracle/next/one/Repositories/TransactionRepository.java
@@ -0,0 +1,45 @@
+package com.oracle.next.one.Repositories;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.oracle.next.one.Models.Transaction;
+
+@Repository
+public interface TransactionRepository extends JpaRepository< Transaction,Long> {
+
+//	 /**
+//     * Busca todas as transações realizadas por um usuário específico.
+//     * 
+//     * @param userId ID do usuário que realizou as transações.
+//     * @return Lista de transações realizadas pelo usuário.
+//     */
+//    List<Transaction> findByUserId(Long userId);
+//    
+//    /**
+//     * Busca todas as transações realizadas em uma data específica.
+//     * 
+//     * @param date Data em que as transações foram realizadas.
+//     * @return Lista de transações realizadas na data especificada.
+//     */
+//    List< Transaction> findByDate(LocalDate date);
+//    
+//    /**
+//     * Busca todas as transações realizadas por um usuário específico em uma data específica.
+//     * 
+//     * @param userId ID do usuário que realizou as transações.
+//     * @param date Data em que as transações foram realizadas.
+//     * @return Lista de transações realizadas pelo usuário na data especificada.
+//     */
+//    List< Transaction> findByUserIdAndDate(Long userId, LocalDate date);
+
+}
+
+
+
+
+
+
