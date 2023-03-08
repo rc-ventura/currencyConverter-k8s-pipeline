a40f23f6deb46d3bec033da72371de75d2377c44
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2023-03-08 01:09:18 -0300

    Configuration: Update Pom and Application

---
+++ src/main/java/com/oracle/next/one/CurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.next.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+
+}
