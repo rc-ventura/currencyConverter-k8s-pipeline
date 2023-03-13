1574f44a2f54d0f6d8720451db1e8f2899884f8e
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2023-03-13 08:14:33 -0300

    Feature: Controlador de exceções criado e testado

---
+++ src/main/java/com/oracle/next/one/Exceptions/CurrencyExceptionHandler.java
@@ -0,0 +1,94 @@
+package com.oracle.next.one.Exceptions;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+/**
+ * Classe responsável por controlar exceções lançadas pelas requisições da aplicação.
+ */
+
+
+@ControllerAdvice
+public class CurrencyExceptionHandler  extends ResponseEntityExceptionHandler {
+	
+
+	    /**
+	     * Método responsável por tratar exceções genéricas.
+	     * @param ex exceção lançada
+	     * @param request requisição recebida
+	     * @return objeto ResponseEntity contendo informações sobre a exceção
+	     */
+	    @ExceptionHandler(value = { Exception.class })
+	    public ResponseEntity<Object> handleGenericException(Exception ex, WebRequest request) {
+	        String errorMessage = "An error occurred while processing your request.";
+	        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+	        return handleExceptionInternal(ex, errorMessage, new HttpHeaders(), status, request);
+	    }
+
+	    /**
+	     * Método responsável por tratar exceções de resposta com status.
+	     * @param ex exceção lançada
+	     * @param request requisição recebida
+	     * @return objeto ResponseEntity contendo informações sobre a exceção
+	     */
+	    @ExceptionHandler(value = { ResponseStatusException.class })
+	    public ResponseEntity<Object> handleResponseStatusException(ResponseStatusException ex, WebRequest request) {
+	        String errorMessage = ex.getReason();
+	        HttpStatus status = (HttpStatus) ex.getStatusCode();
+
+	        return handleExceptionInternal(ex, errorMessage, new HttpHeaders(), status, request);
+	    }
+
+	    /**
+	     * Método responsável por tratar exceções de argumentos inválidos.
+	     * @param ex exceção lançada
+	     * @param request requisição recebida
+	     * @return objeto ResponseEntity contendo informações sobre a exceção
+	     */
+	    @ExceptionHandler(value = { IllegalArgumentException.class })
+	    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex, WebRequest request) {
+	        String errorMessage = "Invalid request parameter.";
+	        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+	        return handleExceptionInternal(ex, errorMessage, new HttpHeaders(), status, request);
+	    }
+
+	    /**
+	     * Método responsável por tratar exceções de recursos não encontrados.
+	     * @param ex exceção lançada
+	     * @param request requisição recebida
+	     * @return objeto ResponseEntity contendo informações sobre a exceção
+	     */
+	    @ExceptionHandler(value = { NoSuchElementException.class })
+	    public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException ex, WebRequest request) {
+	        String errorMessage = "The requested resource was not found.";
+	        HttpStatus status = HttpStatus.NOT_FOUND;
+
+	        return handleExceptionInternal(ex, errorMessage, new HttpHeaders(), status, request);
+	    }
+
+	    /**
+	     * Método responsável por tratar exceções de violação de integridade de dados.
+	     * @param ex exceção lançada
+	     * @param request requisição recebida
+	     * @return objeto ResponseEntity contendo informações sobre a exceção
+	     */
+	    @ExceptionHandler(value = { DataIntegrityViolationException.class })
+	    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+	        String errorMessage = "The requested operation cannot be completed due to data integrity violation.";
+	        HttpStatus status = HttpStatus.CONFLICT;
+
+	        return handleExceptionInternal(ex, errorMessage, new HttpHeaders(), status, request);
+	    }
+	}
