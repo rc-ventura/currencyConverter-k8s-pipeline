8f7f8921bd56c95d9dbba9fece836e1010b97acc
rc-ventura <87483916+rc-ventura@users.noreply.github.com>
2023-04-13 18:57:06 -0300

    feat: terraform-Iac

---
+++ terraform/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "2.27.1"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.personal_token
+}
+
+# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_droplet" "jenkins" {
+  image    = "ubuntu-18-04-x64"
+  name     = "jenkins"
+  region   = var.region
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key_name.id]
+
+}
+
+data "digitalocean_ssh_key" "ssh_key_name" {
+  name = var.ssh_key_name
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name    = "k8s"
+  region  = var.region
+  version = "1.26.3-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
+
+variable "ssh_key_name" {
+  default = ""
+}
+
+variable "personal_token" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+
+}
+
+output "jenkins_ip" {
+    value = digitalocean_droplet.jenkins.ipv4_address
+  
+}
+
+resource  "local_file" "kube_config" {
+    content = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+    filename = "kube_config.yaml" 
+  
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+ssh_key_name   = "digital_ocean-currencyConverter-terraform"
+personal_token = "dop_v1_63eb6ab99b363cad069137fd3ff0c182ee3956bb97095320fa161690d1aa6ebf"
+region         = "nyc1"
